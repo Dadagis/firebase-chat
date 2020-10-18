ddcda9f8c6595e877139c509cfd52b99cb729e83
David <davidobob@laposte.net>
2020-10-18 11:28:59 +0200

    Updates

---
+++ precache-manifest.e32b19c66be9c85b2cda3cfc0022c55e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea38d94c7f985ec095ea37482d032611",
+    "url": "/firebase-chat/index.html"
+  },
+  {
+    "revision": "bac5a1cb8042efd2dcb4",
+    "url": "/firebase-chat/static/css/main.62f8c841.chunk.css"
+  },
+  {
+    "revision": "49a92d12a20ff6be42d0",
+    "url": "/firebase-chat/static/js/2.f6a6c6d2.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/firebase-chat/static/js/2.f6a6c6d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac5a1cb8042efd2dcb4",
+    "url": "/firebase-chat/static/js/main.83409d0c.chunk.js"
+  },
+  {
+    "revision": "161b7f8f1729ff8bfe37",
+    "url": "/firebase-chat/static/js/runtime-main.1adb0c3e.js"
+  }
+]);
